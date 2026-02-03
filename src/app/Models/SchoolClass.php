8630a613d0ef40c6b93ebb1075675ac39c1917d7
reddcs <chakirm082@gmail.com>
2026-02-03 16:44:23 +0100

    feat: add relationships and fillable attributes to SchoolClass model

--- src/app/Models/SchoolClass.php
+++ src/app/Models/SchoolClass.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class SchoolClass extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    public function student(){
+        return $this->hasMany(User::class, 'class_id')
+        ->where('role', 'student');
+    }
+
+    public function teacher(){
+        return $this->belongsToMany(User::class, 'class_teacher', 'class_id', 'teacher_id');
+    }
+
+    public function breifs(){
+        return $this->hasMany(Brief::class);
+    }
+
+    public function sprints(){
+        return $this->belongsToMany(Sprint::class, 'class_sprint');
+    }
 }
