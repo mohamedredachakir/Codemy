cdc1753304fb68405513b3979885493134ddb279
reddcs <chakirm082@gmail.com>
2026-02-03 16:44:18 +0100

    feat: define fillable attributes and relationships in Evaluation model

--- src/app/Models/Evaluation.php
+++ src/app/Models/Evaluation.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Evaluation extends Model
 {
-    //
+    protected $fillable = [
+        "student_id",
+        "brief_id",
+        "competence_id",
+        "teacher_id",
+        "level",
+        "comment",
+        "evaluated_at"
+    ];
+
+
+    protected $casts = [
+        'level' => EvaluationLevelEnum::class
+    ];
+
+
+    public function student() {
+        return $this->belongsTo(User::class, "student_id");
+    }
+
+    public function teacher(){
+        return $this->belongsTo(User::class,"teacher_id");
+    }
+
+    public function brief() {
+        return $this->belongsTo(Brief::class);
+    }
+
+    public function competence(){
+        return $this->belongsTo(Competence::class);
+    }
 }
