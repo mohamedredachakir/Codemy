df4ebed8b61929e81b9774a71686050a316168ba
reddcs <chakirm082@gmail.com>
2026-02-03 21:00:43 +0100

    feat: remove unnecessary id column from class_teacher migration

--- src/database/migrations/2026_02_03_112839_create_class_teacher_table.php
+++ src/database/migrations/2026_02_03_112839_create_class_teacher_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('class_teacher', function (Blueprint $table) {
-            $table->id();
             $table->foreignId('class_id')->constrained()->cascadeOnDelete();
             $table->foreignId('teacher_id')->constrained('users')->cascadeOnDelete();
             $table->primary(['class_id','teacher_id']);
