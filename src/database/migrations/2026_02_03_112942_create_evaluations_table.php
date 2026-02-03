d3bcabf5793177d7c2c473b8bd8fa27e911d1a8e
reddcs <chakirm082@gmail.com>
2026-02-03 15:44:42 +0100

    feat: create evaluations table with foreign keys for student_id, brief_id, competence_id, and teacher_id, and add level, comment, and evaluated_at columns

--- src/database/migrations/2026_02_03_112942_create_evaluations_table.php
+++ src/database/migrations/2026_02_03_112942_create_evaluations_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\EvaluationLevelEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,19 @@ return new class extends Migration
     {
         Schema::create('evaluations', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('student_id')->constrained('users');
+            $table->foreignId('brief_id')->constrained('briefs');
+            $table->foreignId('competence_id')->constrained('comptences');
+            $table->foreignId('teacher_id')->constrained('users');
+
+            $table->enum('level', [EvaluationLevelEnum::IMITER,
+                                                    EvaluationLevelEnum::S_ADAPTER,
+                                                    EvaluationLevelEnum::TRANSPOSER]);
+
+            $table->text('comment')->nullable();
+            $table->timestamp('evaluated_at')->nullable();
+
             $table->timestamps();
         });
     }
