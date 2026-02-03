6fac3f51731df6866b4238ccf6f8a2de9e9636f1
reddcs <chakirm082@gmail.com>
2026-02-03 17:24:38 +0100

    feat: implement AuthController with login, logout, and showLoginForm methods

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    
+    public function showLoginForm(){
+
+    }
+
+    public function login(Request $request){
+
+    }
+
+    public function logout(){
+
+    }
 }
