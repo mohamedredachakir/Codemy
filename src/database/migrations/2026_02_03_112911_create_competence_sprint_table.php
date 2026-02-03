9d17e1d1bbb2b30d530b2e9eb6d5decb26807f01
reddcs <chakirm082@gmail.com>
2026-02-03 21:00:56 +0100

    feat: remove unnecessary id column from competence_sprint migration

--- src/database/migrations/2026_02_03_112911_create_competence_sprint_table.php
+++ src/database/migrations/2026_02_03_112911_create_competence_sprint_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('competence_sprint', function (Blueprint $table) {
-            $table->id();
             $table->foreignId('competence_id')->constrained()->cascadeOnDelete();
             $table->foreignId('sprint_id')->constrained()->cascadeOnDelete();
             $table->primary(['competence_id','sprint_id']);
