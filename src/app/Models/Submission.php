6295c85f6623997bb2d206286b27d535d43d004a
reddcs <chakirm082@gmail.com>
2026-02-03 16:43:59 +0100

    feat: implement Submission model with relationships to User and Evaluation

--- src/app/Models/Submission.php
+++ src/app/Models/Submission.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Submission extends Model
 {
-    //
+    protected $fillable = [
+        'student_id',
+        'brief_id',
+        'content',
+        'submitted_at'
+    ];
+
+
+    public function student(){
+        return $this->belongsTo(User::class, 'student_id');
+    }
+
+    public function brief(){
+        return $this->belongsTo(User::class,);
+    }
+
+    public function evaluation(){
+        return $this->hasOne(Evaluation::class);
+    }
 }
