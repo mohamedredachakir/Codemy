b28479f0e4322412c37835d194d6cf3e1d9a4517
reddcs <chakirm082@gmail.com>
2026-02-03 16:44:12 +0100

    feat: add Competence model with fillable attributes and relationships to Sprint and Evaluation

--- src/app/Models/Competence.php
+++ src/app/Models/Competence.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Competence extends Model
 {
-    //
+    protected $fillable = [
+        'code',
+        'label'
+    ];
+
+    public function sprints(){
+        return $this->belongsToMany(Sprint::class,'competence_sprint');
+    }
+
+    public function evaluations(){
+        return $this->hasMany(Evaluation::class);
+    }
 }
