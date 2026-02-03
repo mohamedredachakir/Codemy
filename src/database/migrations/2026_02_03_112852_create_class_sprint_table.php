626cf838a53e237d238a81718f0d877bed5a1a96
reddcs <chakirm082@gmail.com>
2026-02-03 15:44:19 +0100

    feat: create class_sprint table with foreign keys for class_id and sprint_id

--- src/database/migrations/2026_02_03_112852_create_class_sprint_table.php
+++ src/database/migrations/2026_02_03_112852_create_class_sprint_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('class_sprint', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('class_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('sprint_id')->constrained('sprints')->cascadeOnDelete();
+            $table->primary(['class_id','sprint_id']);
             $table->timestamps();
         });
     }
