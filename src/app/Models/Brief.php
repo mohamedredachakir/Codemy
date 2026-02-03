1ff8df23ef2979875aa24f9c66f38ab52c6553f2
reddcs <chakirm082@gmail.com>
2026-02-03 16:44:07 +0100

    feat: enhance Brief model with fillable attributes and relationships to Sprint, Schoolclass, User, Submission, and Evaluation

--- src/app/Models/Brief.php
+++ src/app/Models/Brief.php
@@ -2,9 +2,42 @@
 
 namespace App\Models;
 
+use App\Enums\BriefTypeEnum;
 use Illuminate\Database\Eloquent\Model;
 
 class Brief extends Model
 {
-    //
+    protected $fillable = [
+        "title",
+        "description",
+        "estimated_time",
+        "type","sprint_id",
+        "class_id",
+        "teacher_id"
+    ];
+
+
+    protected $casts = [
+        'type' => BriefTypeEnum::class,
+    ];
+
+
+    public function sprint(){
+        return $this->belongsTo(Brief::class);
+    }
+
+    public function schoolclass(){
+        return $this->belongsTo(Schoolclass::class, 'class_id');
+    }
+
+    public function teacher(){
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
+
+    public function submissions(){
+        return $this->hasMany(Submission::class);
+    }
+    public function evaluations(){
+        return $this->hasMany(Evaluation::class);
+    }
 }
