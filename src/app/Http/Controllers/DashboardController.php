750eb202b97407994629434fef6451c27c8c4a8f
reddcs <chakirm082@gmail.com>
2026-02-04 12:26:31 +0100

    update dashboard

--- src/app/Http/Controllers/DashboardController.php
+++ src/app/Http/Controllers/DashboardController.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        
+        $user = auth()->user();
+
+        if($user->isAdmin()){
+            return view("dashboard.admin");
+        }
+        if($user->isTeacher()){
+            return view("dashboard.teacher");
+        }
+        if($user->isStudent()){
+            return view("dashboard.student");
+        }else {
+            return "error! no access no pages for you!";
+        }
     }
 }
