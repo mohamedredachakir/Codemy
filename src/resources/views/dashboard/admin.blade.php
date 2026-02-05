c02fca74c0bc6cf33c866ab4d08033dca5682051
reddcs <chakirm082@gmail.com>
2026-02-05 17:04:57 +0100

    feat(dashboard): add admin dashboard and base app layout

---
+++ src/resources/views/dashboard/admin.blade.php
@@ -0,0 +1,6 @@
+<h1>Admin Dashboard</h1>
+
+<a href="{{ route('users.index') }}">Manage Users</a>
+<a href="{{ route('classes.index') }}">Manage Classes</a>
+<a href="{{ route('sprints.index') }}">Manage Sprints</a>
+<a href="{{ route('competences.index') }}">Manage Competences</a>
