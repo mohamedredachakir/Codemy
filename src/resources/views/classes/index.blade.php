3be2658b6a8fb750cd95b682a83eeb30f4f40439
reddcs <chakirm082@gmail.com>
2026-02-05 20:17:23 +0100

    feat(view): add action buttons (show, edit, delete) to classes list

--- src/resources/views/classes/index.blade.php
+++ src/resources/views/classes/index.blade.php
@@ -3,13 +3,14 @@
 @section('content')
 <h1>Classes List</h1>
 
-<a href="{{ route('classes.create') }}">Create Class</a>
+<a href="{{ route('classes.create') }}" style="margin-bottom:10px; display:inline-block;">Create Class</a>
 
 <table border="1" cellpadding="10">
     <thead>
         <tr>
             <th>ID</th>
             <th>Name</th>
+            <th>Actions</th> 
         </tr>
     </thead>
     <tbody>
@@ -17,6 +18,22 @@
         <tr>
             <td>{{ $class->id }}</td>
             <td>{{ $class->name }}</td>
+            <td>
+
+                <a href="{{ route('classes.show', $class->id) }}" style="margin-right:5px;">Show</a>
+
+
+                <a href="{{ route('classes.edit', $class->id) }}" style="margin-right:5px;">Edit</a>
+
+
+                <form action="{{ route('classes.destroy', $class->id) }}" method="POST" style="display:inline-block;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" onclick="return confirm('Are you sure you want to delete this class?');">
+                        Delete
+                    </button>
+                </form>
+            </td>
         </tr>
         @endforeach
     </tbody>
