1c48c51d0d460522181db0ea411fcc4ebc6351b7
reddcs <chakirm082@gmail.com>
2026-02-06 12:54:39 +0100

    refactor(view): clean up teacher dashboard by removing redundant briefs list

--- src/resources/views/dashboard/teacher.blade.php
+++ src/resources/views/dashboard/teacher.blade.php
@@ -23,25 +23,6 @@
         <a href="{{ route('evaluations.index') }}?class_id={{ $class->id }}">
             Evaluate Students
         </a>
-
-        {{-- List all briefs for this class --}}
-        <h4>Briefs:</h4>
-        <ul>
-            @foreach($class->briefs as $brief)
-                <li>
-                    <a href="{{ route('briefs.show', $brief->id) }}">
-                        {{ $brief->title }}
-                    </a>
-                    {{-- Optional: edit brief --}}
-                    <a href="{{ route('briefs.edit', $brief->id) }}" style="margin-left:10px;">
-                        Edit
-                    </a>
-                </li>
-            @endforeach
-            @if($class->briefs->isEmpty())
-                <li>No briefs yet.</li>
-            @endif
-        </ul>
     </div>
 @endforeach
 
